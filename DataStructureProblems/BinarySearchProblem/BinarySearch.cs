87734b343b60262994e03e2394410635f4b079c2
Anilrocky <anilkumarac96@gmail.com>
2023-03-24 19:25:07 +0530

    [Anil] Add:Binary search the word from word list

---
+++ DataStructureProblems/BinarySearchProblem/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblems.BinarySearchProblem
+{
+    public class BinarySearch
+    {
+        public void ReadFile(string filePath)
+        {
+            string readData = File.ReadAllText(filePath);
+            string[] words = readData.Split(' ');
+            foreach (var data in words)
+            {
+                Console.Write(data+" ");
+            }
+            Console.WriteLine("\nEnter word to search");
+            string search = Console.ReadLine();
+            foreach (var data in words)
+            {
+                if (data.ToLower().Equals(search))
+                {
+                    Console.WriteLine("{0}--> word is found",data);
+                    return;
+                }
+            }
+            Console.WriteLine("Word is not found");
+        }
+    }
+}
--- DataStructureProblems/Program.cs
+++ DataStructureProblems/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructureProblems.BinarySearchProblem;
+using System;
 namespace DataStructureProblems
 {
     class Program
@@ -6,6 +7,22 @@ namespace DataStructureProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure Problems");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Binary search \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch search = new BinarySearch();
+                        search.ReadFile(@"D:\BridgeLabz\DataStructureProblems\DataStructureProblems\BinarySearchProblem\File.txt");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
