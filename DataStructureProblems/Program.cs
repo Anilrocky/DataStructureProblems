ee888018a51b4c3219a3829b7cfd2cd4ade4831b
Anilrocky <anilkumarac96@gmail.com>
2023-03-24 20:11:53 +0530

    [Anil] Add:Read integers and print in sorted order

--- DataStructureProblems/Program.cs
+++ DataStructureProblems/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DataStructureProblems.BinarySearchProblem;
+using DataStructureProblems.BubbleSortProblem;
 using DataStructureProblems.InsertionSortProblem;
 using System;
 namespace DataStructureProblems
@@ -11,7 +12,7 @@ namespace DataStructureProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose option to perform \n1.Binary search \n2.Insertion sort \n3.Exit");
+                Console.WriteLine("\nChoose option to perform \n1.Binary search \n2.Insertion sort \n3.Bubble sort \n4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -24,6 +25,10 @@ namespace DataStructureProblems
                         sort.ReadFile(@"D:\BridgeLabz\DataStructureProblems\DataStructureProblems\BinarySearchProblem\File.txt");
                         break;
                     case 3:
+                        BubbleSort bubbleSort = new BubbleSort();
+                        bubbleSort.ReadFile(@"D:\BridgeLabz\DataStructureProblems\DataStructureProblems\BubbleSortProblem\FileInteger.txt");
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
